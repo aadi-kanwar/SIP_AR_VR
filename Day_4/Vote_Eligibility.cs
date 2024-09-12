ebb15ef9e536a1b92a2952b2e9d6dc7c4b8754ee
Aadi Kanwar <aadi.kanwar@gmail.com>
2024-09-12 18:39:24 +0530

    Vote Eligibility

---
+++ Day_4/Vote_Eligibility.cs
@@ -0,0 +1,17 @@
+// Program to check if a person is eligible to vote or not.
+// 12.09.2024
+
+using System; 
+public class Vote 
+{ 
+    public static void Main() 
+    { 
+        int age = 20; 
+        if (age >= 18) 
+            Console.WriteLine("You are an adult !"); 
+
+        else 
+            Console.WriteLine("Too young to vote !"); 
+    } 
+
+} 
